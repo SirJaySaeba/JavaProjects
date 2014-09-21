499879aea706b79b9d777734f4a252714ada1f62
sirjay <sirjaysan@gmx.de>
2014-09-21 11:08:26 +0200

    chessgame02

---
+++ ChessGame/src/ch01/ChangeGameStateButtonActionListener.java
@@ -0,0 +1,18 @@
+package ch01;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChangeGameStateButtonActionListener implements ActionListener {
+	 
+    private ChessGui chessGui;
+ 
+    public ChangeGameStateButtonActionListener(ChessGui chessGui) {
+        this.chessGui = chessGui;
+    }
+ 
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        this.chessGui.changeGameState();
+    }
+}
