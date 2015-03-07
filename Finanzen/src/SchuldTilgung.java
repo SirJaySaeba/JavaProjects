d91892386bc558c5a8331927d5d715d16c835f09
sirjay <sirjaysan@gmx.de>
2015-03-07 17:04:26 +0100

    update Schuldentilgung

--- Finanzen/src/SchuldTilgung.java
+++ Finanzen/src/SchuldTilgung.java
@@ -8,58 +8,52 @@ public class SchuldTilgung {
 	private static final double DARLEHEN = 50000;
 	private static final double ZINS = 0.0155d;
 	private static final double RATE = 400.42;
-	private static final double sondertilgung = 2500.0;
+	private static final double SONDERTILGUNG = 2500.0;
 	
-	private static double getRestschuld(double restschuld, double zinssumme, double anteil_zins, double anteil_tilgung){
-		anteil_zins = (restschuld * ZINS)/12;
-		zinssumme += (restschuld * ZINS)/12;
-		anteil_tilgung = RATE - anteil_zins;
+	private static double getRestschuld(double restschuld, int laufzeit){
+		double anteil_zins = (restschuld * ZINS)/12;
+		double anteil_tilgung = RATE - anteil_zins;
 		restschuld -= anteil_tilgung;
-		
+		if(laufzeit%12==0){
+			restschuld-=SONDERTILGUNG;
+		}
 		return restschuld;
+	}
+	
+	private static double getZinsSumme(double restschuld, double zinssumme){
+		zinssumme += (restschuld * ZINS)/12;
+		return zinssumme;
 	}
 	
 	public static void main(String[] args){
 		double restschuld = DARLEHEN;
-		double anteil_zins;
-		double anteil_tilgung;
 		double zinssumme=0;
 		int laufzeit = Integer.parseInt(JOptionPane.showInputDialog("Gib eine Laufzeit ein"));
 
 		DecimalFormat df = new DecimalFormat("##.##");
 		
-		
 		if(laufzeit > 0){
 			while(laufzeit > 0){
-				anteil_zins = (restschuld * ZINS)/12;
-				zinssumme += (restschuld * ZINS)/12;
-				anteil_tilgung = RATE - anteil_zins;
-				restschuld -= anteil_tilgung;
+				restschuld = getRestschuld(restschuld, laufzeit);
+				zinssumme = getZinsSumme(restschuld, zinssumme);
 				laufzeit--;
-				if(laufzeit%12==0){
-					restschuld-=sondertilgung;
-				}
 				System.out.println(restschuld);
 			}
-			System.out.println("gezahlte Zinsen: "+df.format(zinssumme)+" €");
-			System.out.println("Am Ende der Laufzeit bleiben "+df.format(restschuld) +" € übrig.");
+			System.out.println("gezahlte Zinsen: "+df.format(zinssumme)+" â‚¬");
+			System.out.println("Am Ende der Laufzeit bleiben "+df.format(restschuld) +" Ã¼brig.");
 		}else{
 			while(restschuld > 0){
-				anteil_zins = (restschuld * ZINS)/12;
-				zinssumme += (restschuld * ZINS)/12;
-				anteil_tilgung = RATE - anteil_zins;
-				restschuld -= anteil_tilgung;
+				restschuld = getRestschuld(restschuld,laufzeit);
+				zinssumme = getZinsSumme(restschuld, zinssumme);
 				laufzeit++;
-				if(laufzeit%12==0){
-					restschuld-=sondertilgung;
-				}
+
 				System.out.println(restschuld);
 			}
 			
 			int jahre = laufzeit/12;
 			int monate = laufzeit%12;
-			System.out.println("gezahlte Zinsen: "+df.format(zinssumme)+" €");
-			System.out.println("Die Tilgung würde "+jahre+" Jahre und "+monate+" Monate dauern");
+			System.out.println("gezahlte Zinsen: "+df.format(zinssumme)+" â‚¬");
+			System.out.println("Die Tilgung wÃ¼rde "+jahre+" Jahre und "+monate+" Monate dauern");
 		}
 		
 	}
